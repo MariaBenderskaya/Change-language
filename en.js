cec9ec0d4aa5ee4007f870fc0d18d113c5b254c9
Maria Benderskaya <115712472+MariaBenderskaya@users.noreply.github.com>
2024-05-28 13:03:30 +0700

    Create en.js

---
+++ en.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    key: "Change language",
+    title: "Sign up",
+    login: "Login",
+    mail: "e-mail",
+    password: "password",
+    password2: "re-enter the password",
+    language: "language: select a language",
+    languageRus: "Russian language",
+    languageEng: "English language",
+    checkbox: "Agree with the Terms of Service",
+    submit: "Register",
+    reset: "Reset",
+  },
+};
