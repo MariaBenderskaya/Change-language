d00fc6cefee5efd147087bf9d9f5c1a4a45e9eea
Maria Benderskaya <115712472+MariaBenderskaya@users.noreply.github.com>
2024-05-28 13:04:15 +0700

    ru.js

---
+++ ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    key: "Сменить язык",
+    title: "Регистрация",
+    login: "Логин",
+    mail: "Электронная почта",
+    password: "Пароль",
+    password2: "Пароль повторно",
+    language: "Выберите язык:",
+    languageRus: "Русский язык",
+    languageEng: "Английский язык",
+    checkbox: "Согласен с Условиями обслуживания",
+    submit: "Регистрация",
+    reset: "Сброс",
+  },
+};
