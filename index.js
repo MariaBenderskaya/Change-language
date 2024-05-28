c270b589ba8ee32f45c4bd753976f767fe2a0fcc
Maria Benderskaya <115712472+MariaBenderskaya@users.noreply.github.com>
2024-05-28 13:00:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import ru from "./language/ru.js";
+import en from "./language/en.js";
+
+const btn = document.getElementById("changeLang");
+const output = (err, t) => {
+  if (err) {
+    return console.log("something went wrong loading", err);
+  }
+  btn.innerHTML = t("key");
+  document.getElementById("pageTitle").innerHTML = t("title");
+  document.getElementById("login").innerHTML = t("login");
+  document.getElementById("mail").innerHTML = t("mail");
+  document.getElementById("password").innerHTML = t("password");
+  document.getElementById("password2").innerHTML = t("password2");
+  document.getElementById("selectLanguage").innerHTML = t("language");
+  document.getElementById("russian").innerHTML = t("languageRus");
+  document.getElementById("english").innerHTML = t("languageEng");
+  document.getElementById("checkboxText").innerHTML = t("checkbox");
+  document.getElementById("submit").innerHTML = t("submit");
+  document.getElementById("reset").innerHTML = t("reset");
+};
+
+i18next.init(
+  {
+    lng: "en",
+    debug: true,
+    resources: {
+      en,
+      ru,
+    },
+  },
+  output
+);
+
+btn.onclick = () => {
+  const lang = i18next.language === "en" ? "ru" : "en";
+  i18next.changeLanguage(lang, output);
+};
